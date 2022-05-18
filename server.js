d731e0828913bc1d91b1924d69012e4eaa583dbe
Isaac Chung <chung.isaac3@gmail.com>
2022-05-18 16:56:28 -0700

    fixed conflict

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ const express = require("express"); // npm installed
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 app.use(express.static(path.join(__dirname, "/client/dist")));
 // other configuration...
+app.use((req, res, next) => {
+  req.headers.Authorization = process.env.AUTH
+  next()
+})
+
+//routes
+app.get('/test', (req, res) => {
+  console.log(req.headers)
+  res.send('hi')
+})
+
+app.post('/test', (req, res) => {
+  console.log(req.headers)
+  res.send('hi')
+})
 
 const PORT = process.env.PORT || 3000;
 
