d62ab9b95ff987a6b4c7bfa475071d30ed2a564b
Isaac Chung <chung.isaac3@gmail.com>
2022-05-18 12:24:59 -0700

    updated webpack dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
+  mode: "development",
   entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'client/dist'),
