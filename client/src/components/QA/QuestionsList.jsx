7de8b1ca6a999d9f4a35b3cbadbae31a644a76c9
Aaron Bowers <bowersaaronjames@gmail.com>
2022-05-19 16:47:11 -0700

    add react-modal to dependencies

--- client/src/components/QA/QuestionsList.jsx
+++ client/src/components/QA/QuestionsList.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect, useContext } from 'react';
 import { Context } from '../util/context.js';
+import styled from 'styled-components';
 
 const QuestionsList = (props) => {
   const id = useContext(Context).id;
 
+  const Search = styled.input`
+
+  `;
+
   return (
     <div>
       <h3>QUESTIONS &#38; ANSWERS</h3>
+      <input placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." size="50"/>
     </div>
   )
 }
