6d49298d4f47302a01a0abedaa9a2b6adeaf2cfb
skydodle <skydodle@gmail.com>
2022-05-20 19:53:04 -0500

    productList carousel initialized

--- client/src/components/RelatedItems/ProductsList.jsx
+++ client/src/components/RelatedItems/ProductsList.jsx
@@ -1,6 +1,8 @@
 import { React, useState, useEffect, useContext } from 'react';
 import { Context } from '../util/context.js';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import ProductCard from './ProductCard.jsx';
+
 
 const ProductsList = ({list}) => {
   const id = useContext(Context).id;
@@ -8,8 +10,9 @@ const ProductsList = ({list}) => {
   return (
     <div className="carousel">
       <div className="inner" style={{ transform: "translateX(-0%" }}>
-        {React.Children.map(list, (child, index) => {
-          return React.cloneElemenet(child, { width: "100%" });
+        {list.map((product, index) => {
+          return <ProductCard product={product} id={index} width={{ width: "100%"}} />
+          // return React.cloneElemenet(child, { width: "100%" });
         })}
       </div>
     </div>
