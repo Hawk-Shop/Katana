8b17c5bb933803bc8cfcc25b7093b2004ddf1e3e
Jessica Zhou <jessicazhou541@gmail.com>
2022-05-25 18:26:14 -0700

    updated form with characteristics and also formats in an object

--- client/src/components/R&R/NewReview.jsx
+++ client/src/components/R&R/NewReview.jsx
@@ -35,10 +35,13 @@ const ModalContent = styled.div`
   background: #f1f1f1;
   padding: 14px 28px;
   border-radius: 10px;
-  max-width: 600px;
+  max-height: 80%;
+  min-height: 300px;
+  max-width: 100%;
   min-width: 300px;
   border-style: solid;
   border-width: 2px;
+  overflow: auto;
 `;
 
 const CloseModalButton = styled.button`
@@ -53,6 +56,32 @@ const Question = styled.div`
 padding: 1em;
 `;
 
+const Option = styled.span`
+display: flex;
+flex-direction: row-reverse;
+align-items: flex-end;
+align-text: left;
+border: dotted red;
+font-size: 1em;
+height: 20%
+`;
+
+const OptionCol = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+border: dotted green;
+margin: 0;
+flex-grow: 1;
+`;
+
+const OptionContainer = styled.div`
+display: flex;
+flex-direction: row;
+border: dotted blue;
+margin: 0;
+`;
+
 const NewReview = ({showModal, setShowModal}) => {
   const id = useContext(Context).id;
   const [rate, setRate] = useState(null); // for stars
@@ -62,11 +91,13 @@ const NewReview = ({showModal, setShowModal}) => {
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
   const [photos, setPhotos] = useState([]);
-  const [characteristics, setCharacteristics] = useState(['Fit']);
+  const [characteristics, setCharacteristics] = useState([]); // array of characteristics
+  const [charObject, setCharObject] = useState({});
+  const [idLegend, setIdLegend] = useState({});
 
   const submitReview = (e) => {
     e.preventDefault;
-    axios.post('/reviews', {
+    let body = {
       product_id: id,
       rating: rate,
       summary: summary,
@@ -75,18 +106,25 @@ const NewReview = ({showModal, setShowModal}) => {
       name: nickname,
       email: email,
       photos: photos,
-      characteristics: characteristics
-    })
+      characteristics: charObject
+    };
+    axios.post('/reviews', body)
+    .then((res) => console.log('successfully added reviews'))
+    .catch((err) => console.log('error', err));
   }
 
   useEffect(() => {
     axios.get(`/reviews/meta/?product_id=${id}`)
     .then((results) => {
-      setCharacteristics(Object.keys(results.data.characteristics));
+      const idLeg = {};
+      let charNames = Object.keys(results.data.characteristics);
+      charNames.forEach((charName) => {
+        idLeg[charName] = results.data.characteristics[charName].id;
+      })
+      setCharacteristics(charNames);
+      setIdLegend(idLeg);
     })
   }, [])
-
-  console.log('MY LEGEND', charLegend)
 
   return (
     <>
@@ -116,30 +154,38 @@ const NewReview = ({showModal, setShowModal}) => {
               </Question>
               <Question>
                 <label htmlFor="characteristics">Characteristics *</label>
-                {characteristics.map((char) => {
-                  let ratings = Object.values(charLegend[char]);
-                  return (
-                    <div>
-                      {char}
-                      <div>
-                        {ratings.map((rating) => (
-                          <span>
-                            <label htmlFor={rating}>{rating}</label>
-                            <input type="radio" name={rating} value={rating}></input>
-                          </span>
-                        ))}
-                      </div>
-                    </div>
-                  )
-                })}
+                <OptionContainer>
+                  {characteristics.map((char) => {
+                    let ratings = Object.keys(charLegend[char]); // ratings are numbers
+                    return (
+                        <OptionCol>
+                          <label> {char}
+                            {ratings.map((rating) => (
+                              <Option>
+                                <label htmlFor={char}>{charLegend[char][rating]}</label>
+                                <input type="radio" name={char} value={rating + char} onChange={(e) => {
+                                  let newCharObject = {...charObject};
+                                  let numRating = e.target.value.substring(0, 1);
+                                  let charName = e.target.value.substring(1);
+                                  let charId = idLegend[charName];
+                                  newCharObject[charId] = numRating;
+                                  setCharObject(newCharObject);
+                                }} required></input>
+                              </Option>
+                            ))}
+                          </label>
+                        </OptionCol>
+                    )
+                  })}
+                </OptionContainer>
               </Question>
               <Question>
                 <label htmlFor="summary">Review summary&nbsp;</label>
-                <textarea name="summary" cols="50" rows="2" maxLength="60" placeholder="Example: Best purchase ever!" onChange={(e) => {setSummary(e.target.value)}}></textarea>
+                <textarea name="summary" cols="60" rows="2" maxLength="60" placeholder="Example: Best purchase ever!" onChange={(e) => {setSummary(e.target.value)}}></textarea>
               </Question>
               <Question>
                 <label htmlFor="body">Review body *&nbsp;</label>
-                <textarea name="body" cols="50" rows="5" minLength="50" maxLength="1000" placeholder="Why did you like the product or not?" required onChange={(e) => {setBody(e.target.value)}}></textarea>
+                <textarea name="body" cols="60" rows="5" minLength="50" maxLength="1000" placeholder="Why did you like the product or not?" required onChange={(e) => {setBody(e.target.value)}}></textarea>
                 {body.length < 50 ? <span>Minimum required characters left: {50-body.length}</span> : <span>Minimum reached</span>}
               </Question>
               <Question>
@@ -148,12 +194,12 @@ const NewReview = ({showModal, setShowModal}) => {
               </Question>
               <Question>
                 <label htmlFor="nickname">What is your nickname *&nbsp;</label>
-                <textarea name="nickname" cols="50" rows="2" maxLength="60" placeholder="Example: jackson11!" required onChange={(e) => {setNickname(e.target.value)}}></textarea>
+                <textarea name="nickname" cols="60" rows="1" maxLength="60" placeholder="Example: jackson11!" required onChange={(e) => {setNickname(e.target.value)}}></textarea>
                 <span>For privacy reasons, do not use your full name or email address</span>
               </Question>
               <Question>
                 <label htmlFor="email">Your email *&nbsp;</label>
-                <textarea name="email" cols="50" rows="2" maxLength="60" placeholder="Example: jackson11@email.com" required onChange={(e) => {setEmail(e.target.value)}}></textarea>
+                <textarea name="email" cols="60" rows="1" maxLength="60" placeholder="Example: jackson11@email.com" required onChange={(e) => {setEmail(e.target.value)}}></textarea>
                 <span>For authentication reasons, you will not be emailed</span>
               </Question>
               <input type="submit" value="Submit review"></input>
