0f86784495e9e0ff619bc40d4a493f6532ffa6a5
Isaac Chung <chung.isaac3@gmail.com>
2022-05-31 11:32:15 -0700

    fixed zoom

--- client/src/components/overview/Gallery.jsx
+++ client/src/components/overview/Gallery.jsx
@@ -15,10 +15,37 @@ const Image = styled.div`
   background-repeat: no-repeat;
   background-size: auto 100%;
   display: flex;
+  background-image: ${(props) => `url("${props.background}")`};
   &.zoomed {
-    transform: scale(5);
-    height: 50%;
-    position: relative;
+    transform: scale(2.5);
+    height: 100%;
+    width: 100%;
+    // position: relative;
+    background-size: contain;
+    background-position: center;
+    transform-origin: ${(props) => `${props.xAxis}px ${props.yAxis}px`};
+  }
+`;
+
+const ZoomedImg = styled.img`
+  height: 100%;
+  width: 100%;
+  background-position: 50% 20%;
+  background-repeat: no-repeat;
+  background-size: auto 100%;
+  display: flex;
+
+  &.zoomed {
+    transform: scale(2);
+    height: 100%;
+    width: 100%;
+    // position: relative;
+    background-size: cover;
+    background-position: center;
+    transform-origin: ${(props) => `${props.xAxis}px ${props.yAxis}px`};
+    &.zoomOut {
+      cursor: zoom-out;
+    }
   }
 `;
 
@@ -26,10 +53,13 @@ const ImageCtn = styled.div`
   width: 100%;
   height: 55vh;
   background-color: rgb(0, 0, 0, 0.3);
+
   margin-right: 8%;
   &.zoomed {
     background-color: transparent;
-    width: 30%;
+    overflow: hidden;
+    width: 50%;
+    height: 80vh;
   }
 `;
 
@@ -102,14 +132,14 @@ const Gallery = (props) => {
   let cursor = props.expandedView ? "crosshair" : "zoom";
 
   if (isZoomed) {
-    cursor = "zoomOut"
+    cursor = "zoomOut";
   }
   const handleMouse = (e) => {
-    setX(500 - e.clientX);
-    setY(500 - e.clientY);
+    setX(e.clientX - e.target.offsetLeft);
+    setY(e.clientY - e.target.offsetTop);
     console.log(window.innerWidth, e.clientX);
   };
-
+  console.log(props.currentStyle.photos[currImg].url);
   return (
     <GallFlex className={[expanded, zoomOverflow].join(" ")}>
       {!props.expandedView && (
@@ -121,60 +151,64 @@ const Gallery = (props) => {
       )}
       {props.currentStyle.photos[currImg].url && (
         <ImageCtn className={zoomOverflow}>
-          <Image
-            style={
-              isZoomed
-                ? {
-                    backgroundImage: `url(${props.currentStyle.photos[currImg].url})`,
-                    left: `${x}px`,
-                    top: `${y}px`,
-                  }
-                : {
-                    backgroundImage: `url(${props.currentStyle.photos[currImg].url})`,
-                  }
-            }
-            className={zoomOverflow}
-          >
-            {!isZoomed && (
-              <LeftRight
-                onClick={() => {
-                  currImg > 0 && setCurrImg(currImg - 1);
-                }}
-              >
-                {currImg !== 0 && <Arrow icon={faArrowLeft} />}
-              </LeftRight>
-            )}
-            {props.expandedView ? (
-              <Center
-                onClick={() => setIsZoomed(!isZoomed)}
-                className={[cursor, zoomOverflow].join(" ")}
-                onMouseMove={handleMouse}
-              ></Center>
-            ) : (
-              <Center
-                onClick={() => props.setExpandedView(!props.expandedView)}
-                className={cursor}
-              ></Center>
-            )}
-            {!isZoomed && (
-              <Expand
-                icon={faExpand}
-                onClick={() => props.setExpandedView(!props.expandedView)}
-              />
-            )}
-            {!isZoomed && (
-              <LeftRight
-                onClick={() => {
-                  currImg < props.currentStyle.photos.length - 1 &&
-                    setCurrImg(currImg + 1);
-                }}
-              >
-                {currImg !== props.currentStyle.photos.length - 1 && (
-                  <Arrow icon={faArrowRight} />
-                )}
-              </LeftRight>
-            )}
-          </Image>
+          {isZoomed && (
+            <ZoomedImg
+              src={props.currentStyle.photos[currImg].url}
+              xAxis={x}
+              yAxis={y}
+              onClick={() => setIsZoomed(!isZoomed)}
+              className={[cursor, zoomOverflow].join(" ")}
+              onMouseMove={handleMouse}
+            ></ZoomedImg>
+          )}
+          {!isZoomed && (
+            <Image
+              xAxis={x}
+              yAxis={y}
+              background={props.currentStyle.photos[currImg].url}
+              className={zoomOverflow}
+            >
+              {!isZoomed && (
+                <LeftRight
+                  onClick={() => {
+                    currImg > 0 && setCurrImg(currImg - 1);
+                  }}
+                >
+                  {currImg !== 0 && <Arrow icon={faArrowLeft} />}
+                </LeftRight>
+              )}
+              {props.expandedView ? (
+                <Center
+                  onClick={() => setIsZoomed(!isZoomed)}
+                  className={[cursor, zoomOverflow].join(" ")}
+                  onMouseMove={handleMouse}
+                ></Center>
+              ) : (
+                <Center
+                  onClick={() => props.setExpandedView(!props.expandedView)}
+                  className={cursor}
+                ></Center>
+              )}
+              {!isZoomed && (
+                <Expand
+                  icon={faExpand}
+                  onClick={() => props.setExpandedView(!props.expandedView)}
+                />
+              )}
+              {!isZoomed && (
+                <LeftRight
+                  onClick={() => {
+                    currImg < props.currentStyle.photos.length - 1 &&
+                      setCurrImg(currImg + 1);
+                  }}
+                >
+                  {currImg !== props.currentStyle.photos.length - 1 && (
+                    <Arrow icon={faArrowRight} />
+                  )}
+                </LeftRight>
+              )}
+            </Image>
+          )}
           {props.expandedView && !isZoomed && (
             <IconGall
               currImg={currImg}
