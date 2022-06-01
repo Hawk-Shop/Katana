f5d85f9f672df68da6ab51010043b53f64dd29fe
Jessica Zhou <jessicazhou541@gmail.com>
2022-06-01 12:52:05 -0700

    box shadow

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -26,6 +26,9 @@ min-width: 100%;
 min-height: 100%;
 border-radius: 15px;
 cursor: pointer;
+&:hover {
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
 `;
 
 const HomePage = ({setId, changeView}) => {
