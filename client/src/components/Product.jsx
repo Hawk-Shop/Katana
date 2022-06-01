387a98343ecb381c1e47aefc70be222e65613f87
Isaac Chung <chung.isaac3@gmail.com>
2022-06-01 12:32:11 -0700

    removed unnecessary changes from App and Product

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, Suspense } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { Context } from "./util/context.js";
 import Overview from "./overview/Overview.jsx";
@@ -8,55 +8,15 @@ import RelatedProducts from './RelatedItems/Main.jsx';
 import axios from 'axios';
 
 import {ThemeProvider} from "styled-components";
-import {useDarkMode} from "./DarkMode/UseDarkMode.jsx"
 import { GlobalStyles } from "./DarkMode/GlobalStyles.jsx";
-import { lightTheme, darkTheme } from "./DarkMode/Themes.jsx";
 import Toggle from "./DarkMode/Toggler.jsx";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import HomePage from "./HomePage.jsx";
 
 const StyledApp = styled.div`
   max-width: 80%;
   margin: 0 auto;
   min-height: 100vh;
 `;
-
-const HeaderStyle = styled.header`
-  max-width: 80%;
-  margin: 0 auto;
-`;
-
-const NavBar = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-`;
-
-const List = styled.ul`
-  display: flex;
-  list-style: none;
-  width: 20%;
-  justify-content: flex-end;
-`;
-
-const ListItem = styled.li`
-  margin: 3%;
-  cursor: pointer;
-`;
-
-const CartNum = styled.span`
-  font-size: 0.7rem;
-  position: relative;
-  bottom: 15px;
-  font-weight: bold;
-`;
-
-const FontIcon = styled(FontAwesomeIcon)``;
-
-
 
 const Product= ({id, setId, themeMode, themeToggler, theme , cart, setCart, cartQty, setCartQty, reviewsRef}) => {
 
@@ -67,7 +27,6 @@ const Product= ({id, setId, themeMode, themeToggler, theme , cart, setCart, cart
   .get(`/products/${id}`)
   .then((res) => setProductName(res.data.name))
   .catch((err) => console.log(err));
-console.log("CART", cart);
 
 return (
   <ThemeProvider theme={themeMode}>
