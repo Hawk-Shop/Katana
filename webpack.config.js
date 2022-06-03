2a06f6b566fb1384af5876d7bb1564ab9f2ae073
Isaac Chung <chung.isaac3@gmail.com>
2022-06-03 15:28:13 -0700

    changed development to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  mode: "development",
+  mode: "production",
   entry: './client/src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'client/dist'),
