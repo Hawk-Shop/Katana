62c5eae155b23c6544ee7ba020d21089ef325dd7
Isaac Chung <chung.isaac3@gmail.com>
2022-06-03 10:36:04 -0700

    fixed responsiveness

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -36,8 +36,17 @@ const NavBar = styled.nav`
   z-index: 1;
   position: fixed;
   height: 4em;
-`;
-
+  @media (max-width: 520px) {
+    align-items: baseline;
+    justify-content: space-around;
+  }
+  @media (max-width: 268px) {
+    height: 5em;
+  }
+  @media (max-width: 183px) {
+    height: 6em;
+  }
+`
 const List = styled.ul`
   display: flex;
   list-style: none;
@@ -55,6 +64,10 @@ const CartNum = styled.span`
   position: relative;
   bottom: 15px;
   font-weight: bold;
+  @media (max-width: 442px) {
+    bottom: 32px;
+    left: 25px;
+  }
 `;
 
 const FontIcon = styled(FontAwesomeIcon)``;
